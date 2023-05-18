3beec79852d954582f086d9d1d01230b8a991143
Suzana <grdic.suzana@gmail.com>
2023-05-18 12:02:38 +0200

    render list

--- src/HighLevelContainer.js
+++ src/HighLevelContainer.js
@@ -1,12 +1,28 @@
 import SearchBar from "./SearchBar";
 import ToDoItems from "./ToDoItems";
 import AddItemForm from "./AddItemForm";
+import { useEffect, useState } from "react";
 
 function HighLevelContainer (props) {
+  const [toDoItems, setToDoItems] = useState([]);
+  useEffect(() => {
+    setToDoItems([
+      {
+        id:1,
+        text:'Operi sude',
+        isCompleted: false,
+      },
+      {
+        id:2,
+        text:'Operi ves',
+        isCompleted: false,
+      }
+    ])
+  }, [])
   return (
     <>
       <div><SearchBar/></div>
-      <div><ToDoItems/></div>
+      <div><ToDoItems items={toDoItems}/></div>
       <div><AddItemForm/></div>
     </>
   )
--- src/ToDoItems.js
+++ src/ToDoItems.js
@@ -1,5 +1,15 @@
-function ToDoItems() {
-
+function ToDoItems({items}) {
+  return (
+    <>
+    <ul>
+      {
+        items.map((item) => (
+          <li>{item.text}</li>
+        ))
+      }
+    </ul>
+    </>
+  )
 } ;
 
 export default ToDoItems;
